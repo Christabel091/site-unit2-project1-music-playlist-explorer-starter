6a42246d8e0ecd44a9b8d835fc2fec607492d2a8
Christabel Gosiora Obi-Nwosu <christabelobi@meta.com>
2025-06-05 12:06:20 -0700

    fully implemented behaviour of mvp features

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -2,25 +2,30 @@
 let cards = document.getElementById('playlist-cards');
 let modalOverlay = document.getElementById('modal-overlay');
 let modalContent = document.getElementById('modal-content')
+const modalBody = document.getElementById('modal-body');
 
-//function to display modal
-function loadModal(playlistSong){
-    modalOverlay.style.display = 'flex';
-    modalContent.style.display = 'flex';
-    const modalImage = document.getElementById("modal-image");
-   
-    const modalName = document.getElementById("modal-name");
-    const modalCreator = document.getElementById("modal-creator");
-    modalImage.src = playlistSong.playlist_art;
-    modalName.textContent = playlistSong.playlist_name;
-    modalCreator.textContent = playlistSong.playlist_creator;
+function shuffleArray(arr) {
+    console.log("func ran ")
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+       [arr[i], arr[j]] = [arr[j], arr[i]]
+    }
+    return arr;
+}
 
-    const modalBody = document.getElementById('modal-body');
-    if (modalBody !== ''){modalBody.innerHTML = '';}
-    const button = document.createElement('button');
-    modalBody.appendChild(button);
-    
-    playlistSong.songs.forEach((song) =>{
+function shuffleSongs(Songs){
+    let shuflledSongs =shuffleArray(Songs);
+    console.log(shuflledSongs)
+    modalBody.querySelectorAll('div').forEach((div) => {
+        div.remove();
+    })
+    console.log("modalBody")
+    console.log(modalBody)
+    displaySongs(shuflledSongs);
+}
+
+function displaySongs(PlaylistSongs){
+    PlaylistSongs.forEach((song) =>{
         const modalSong = document.createElement('div');
         modalSong.classList.add('modal-song');
 
@@ -57,6 +62,28 @@ function loadModal(playlistSong){
     })
 }
 
+//function to display modal
+function loadModal(playlistSong){
+    modalOverlay.style.display = 'flex';
+    modalContent.style.display = 'flex';
+    const modalImage = document.getElementById("modal-image");
+   
+    const modalName = document.getElementById("modal-name");
+    const modalCreator = document.getElementById("modal-creator");
+    modalImage.src = playlistSong.playlist_art;
+    modalName.textContent = playlistSong.playlist_name;
+    modalCreator.textContent = playlistSong.playlist_creator;
+    if (modalBody !== ''){modalBody.innerHTML = '';}
+    const button = document.createElement('button');
+    button.id = 'button'
+    button.textContent = 'shuffle';
+    modalBody.appendChild(button);
+    displaySongs(playlistSong.songs);
+    button.addEventListener('click', () =>{
+        shuffleSongs(playlistSong.songs);
+    })
+}
+
 
 //function to retrieve playists data and create playlist card with data
 function displayPlaylist(){
@@ -68,50 +95,51 @@ function displayPlaylist(){
     }).then(
         (Playlists) => {
             Playlists.playlists.forEach(pl => {
-                console.log("data accessed");
                 const card = document.createElement('div');
-                card.classList.add('playlist-card')
+                card.classList.add('playlist-card');
                 card.setAttribute('id', pl.playlistID);
-                
-                const cardImage = document.createElement('img');
-                cardImage.src = pl.playlist_art;
-                cardImage.classList.add('playlist-card-img');
-                card.appendChild(cardImage);
 
-                const cardTitle = document.createElement('h4')
-                cardTitle.classList.add('playlist-card-title');
-                cardTitle.textContent = pl.playlist_name;
-                card.appendChild(cardTitle);
+                card.innerHTML = `
+                <img src="${pl.playlist_art}" class="playlist-card-img">
+                <h4 class="playlist-card-title">${pl.playlist_name}</h4>
+                <h5 class="playlist-card-creator">created by ${pl.playlist_creator}</h5>
+                <div class="card-likes">
+                    <i class="fa-regular fa-heart Heart-icon"></i>
+                    <p>${pl.likeCount}</p>
+                    <div><i class="fa-solid fa-wand-magic-sparkles edit"></i></div>
+                </div>
+                `;
 
-                const cardCreator= document.createElement('h5')
-                cardTitle.classList.add('playlist-card-creator');
-                cardCreator.textContent = "created by "+pl.playlist_creator;
-                card.appendChild(cardCreator);
-                
-                const cardHeartIcon = document.createElement('i');
-                cardHeartIcon.classList.add('fa-regular', 'fa-heart', 'Heart-icon');
-                cardHeartIcon.addEventListener("click", function(){
-                    this.classList.toggle('fa-solid');
-                    this.classList.toggle('fa-regular');
-                    this.classList.toggle('liked');
-                })
-                card.appendChild(cardHeartIcon);
+                const cardLikes = card.querySelector('.card-likes');
+                const cardHeartIcon = cardLikes.querySelector('.Heart-icon');
+                const cardLikeNum = cardLikes.querySelector('p');
+
+                cardHeartIcon.addEventListener("click", function(e) {
+                e.stopPropagation();
+                const isLiked = this.classList.contains('liked');
+                cardLikeNum.textContent = isLiked
+                    ? Number(cardLikeNum.textContent) - 1
+                    : Number(cardLikeNum.textContent) + 1;
+
+                this.classList.toggle('fa-solid');
+                this.classList.toggle('fa-regular');
+                this.classList.toggle('liked');
+                });
+
                 cards.appendChild(card);
-                card.addEventListener('click', () =>{
-                   loadModal(pl);
-                    window.onclick = function(event) {
-                        if ((event.target == modalContent)  || (event.target == modalOverlay)  ) {
-                            modalContent.style.display = "none";
-                            modalOverlay.style.display = "none";
-                        }
+
+                card.addEventListener('click', () => {
+                loadModal(pl);
+
+                window.onclick = function(event) {
+                    if (event.target == modalContent || event.target == modalOverlay) {
+                    modalContent.style.display = "none";
+                    modalOverlay.style.display = "none";
                     }
-                })
-               
-
-
-            });
-        }
-    )
+                }
+                });
+            })
+})
 }
 
 
@@ -119,6 +147,5 @@ function displayPlaylist(){
 
 document.addEventListener("DOMContentLoaded", () =>{
     displayPlaylist();
-
 });
 
